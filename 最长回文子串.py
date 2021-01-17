c66236be9416b621325a7a73db653e4576ced197
energy-100 <abc551218@126.com>
2021-01-17 11:22:09 +0800

    增加 最长回文子串

---
+++ 最长回文子串.py
@@ -0,0 +1,30 @@
+
+def longestPalindrome( s: str) -> str:
+    length = len(s)
+    maxlen = 1
+    startindex = 0
+    bp = [[False for j in range(length)] for i in range(length)]
+    for i in range(length):
+        bp[i][i] = True
+
+    for i in range(1, length):
+        for j in range(i):
+            if (s[j] != s[i]):
+                bp[j][i] = False
+            else:
+                if (i - j <= 2):
+                    bp[j][i] = True
+                else:
+                    bp[j][i] = bp[j + 1][i - 1]
+                if(bp[j][i]==True):
+                    currentMaxLen = i - j + 1
+                    if (currentMaxLen > maxlen):
+                        maxlen = currentMaxLen
+                        startindex = j
+
+    return s[startindex:startindex+maxlen]
+
+
+
+print(longestPalindrome("aacabdkacaa"))
+
