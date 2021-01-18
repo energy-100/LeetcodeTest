069bb05bd42ec736ad9beab012c533e72e2047d4
energy-100 <abc551218@126.com>
2021-01-18 21:53:24 +0800

    增加最长公共子串算法

--- 无重复字符的最长子串.py
+++ 无重复字符的最长子串.py
@@ -1,5 +1,51 @@
 class Solution:
 
+    def lengthOfLongestSubstring_demo(self, s: str) -> int:
+        length=len(s)
+        if(length==0):
+            return 0
+        maxlength=1
+        strtmplist=[s[0]]
+        i=1
+        while(i<length):
+            if(s[i] not in strtmplist):
+                strtmplist.append(s[i])
+                maxlength=max(len(strtmplist),maxlength)
+            else:
+                strtmplist=strtmplist[strtmplist.index(s[i])+1:i]
+                strtmplist.append(s[i])
+            i += 1
+
+        return maxlength
+
+
+
+
+
+    # def lengthOfLongestSubstring(self, s: str) -> int:
+    #     length=len(s)
+    #     slist=[]
+    #     slist.append(s[0])
+    #     maxlen=0
+    #     i=0
+    #     while i < length:
+    #         j=i+1
+    #         while j < length:
+    #             if s[j] not in slist:
+    #                 slist.append(s[j])
+    #                 # j += 1
+    #             else:
+    #                 maxlen = max(maxlen, len(slist))
+    #                 i = i+slist.index(s[j])+1
+    #                 slist=slist[slist.index(s[j])+1:]
+    #                 slist.append(s[j])
+    #             j+=1
+    #             if j==length :
+    #                 break
+    #
+    #     return maxlen
+
+
     def lengthOfLongestSubstring(self, s: str) -> int:
         length = len(s)
         if length <= 1:
@@ -25,29 +71,7 @@ class Solution:
 
         return maxlen
 
-    # def lengthOfLongestSubstring(self, s: str) -> int:
-    #     length=len(s)
-    #     slist=[]
-    #     slist.append(s[0])
-    #     maxlen=0
-    #     i=0
-    #     while i < length:
-    #         j=i+1
-    #         while j < length:
-    #             if s[j] not in slist:
-    #                 slist.append(s[j])
-    #                 # j += 1
-    #             else:
-    #                 maxlen = max(maxlen, len(slist))
-    #                 i = i+slist.index(s[j])+1
-    #                 slist=slist[slist.index(s[j])+1:]
-    #                 slist.append(s[j])
-    #             j+=1
-    #             if j==length :
-    #                 break
-    #
-    #     return maxlen
-
 a=Solution()
-print(a.lengthOfLongestSubstring('au'))
+# print(a.lengthOfLongestSubstring('au'))
+print(a.lengthOfLongestSubstring_demo('aab'))
 # print(a.lengthOfLongestSubstring('abcabcbb'))
--- 最长公共子串.py
+++ 最长公共子串.py
@@ -1,21 +1,3 @@
-# a=[1,2,3,4,5,6]
-# b=[1,2,3,4,5,6]
-
-# print((1,1) in zip(a,b))
-#
-# import numpy as np
-# from collections import Counter
-#
-# data = np.array([1.1, 1.1, 1.1, 2, 3, 5, 4, 4, 4, 5])
-#
-# # 方法一
-# # print('Counter(data)\n', Counter(data))  # 调用Counter函数
-# # print("abc".find())
-# l=list("abcgdaewaa")
-# tmpDict=dict()
-# for elem in l:
-#     tmpDict[elem]
-import re
 
 def demo(text1,text2):
     maxlen = 0
@@ -41,6 +23,3 @@ def demo(text1,text2):
     return maxlen
 
 print(demo("abcde","erbcdet"))
-
-
-
