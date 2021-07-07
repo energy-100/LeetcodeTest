e938771e3791e8e67eb9bf32da2d32a83985f066
energy-100 <abc551218@126.com>
2021-07-07 10:32:54 +0800

    字符串组合穷举算法(修改)

--- 字符串组合穷举.py
+++ 字符串组合穷举.py
@@ -1,17 +1,19 @@
-import copy
 res=[]
 def do(str1,str2):
     if len(str2)==1:
-        tmpstr=str1+str2
+        str1+=str2
         # print(tmpstr)
-        res.append(tmpstr)
-    else:
-        for elem in str2:
-            tmpstr1=str1+elem
-            tmpstr2=str2.replace(elem,'')
-            do(tmpstr1,tmpstr2)
+        res.append(str1)
+        return
+
+    for elem in str2:
+        str1+=elem
+        tmpstr2=str2.replace(elem,'')
+        do(str1,tmpstr2)
 
 
 if __name__ == '__main__':
     do('',"abcdef")
     print(len(res))
+    print(res)
+
